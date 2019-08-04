014a2db29917c6d7a202f8f56baa0cca0637cd1b
wangtianli <tianli9268@163.com>
2019-08-04 22:01:15 +0800

    从practiceOther中提交

---
+++ vueCliTest/src/components/Hi3.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="msg">
+    {{msg}}
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg:'ahahah'
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .msg{
+    background-color: red;
+    color: purple;
+    font-size: 40px;
+  }
+</style>
--- vueCliTest/src/router/index.js
+++ vueCliTest/src/router/index.js
@@ -34,7 +34,7 @@ export default new Router({
           path:'Hi2',
           name:'Hi2',
           component:Hi2
-        },
+        }
       ]
     }
   ]
