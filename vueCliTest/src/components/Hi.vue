a3247dead136061b6149387dd1dfb60a9bfda066
王天丽 <tianli9268@163.com>
2019-11-29 09:37:18 +0800

    Hi.vue添加了一行文字

--- vueCliTest/src/components/Hi.vue
+++ vueCliTest/src/components/Hi.vue
@@ -3,17 +3,19 @@
   <h2>
     {{message}}
     <router-view/>
-
   </h2>
 </div>
 </template>
+
+
 
 <script>
 export default {
   name:'Hi',
   data(){
     return{
-      message:'welcome to inner Mongolia！！,,,,,'
+      message:'welcome to inner Mongolia！！,,,,,',
+      message:'welcome to inner 马里兰州~~~~~~~~~~~'
     }
   }
 }
